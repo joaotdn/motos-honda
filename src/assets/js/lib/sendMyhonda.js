be06009ff7a4b795390d61aec237492995169be9
João Teodoro <joaotdn@gmail.com>
2019-05-13 19:23:40 -0300

    Formulário do myhonda validado com Lolo

--- src/assets/js/lib/sendMyhonda.js
+++ src/assets/js/lib/sendMyhonda.js
@@ -1,5 +1,32 @@
 import $ from "jquery";
 
+export function sendForm() {
+    const headers = new Headers({
+        "Content-Type": "application/x-www-form-urlencoded",
+    });
+
+    const init = {
+        method: 'GET',
+        headers,
+        mode: 'no-cors',
+    };
+
+    const formHonda = $('#myhonda');
+    formHonda.on('submit',  function (event) {
+       event.preventDefault();
+       const data = $(this).serializeArray();
+       console.log(data);
+
+       // fetch('https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8', init)
+       //      .then(res => {
+       //          console.log(res);
+       //      })
+       //      .catch(error => {
+       //          console.log('error');
+       //      });
+    });
+}
+
 export function getProductName() {
     const dataProduct = $('*[data-product]');
     if(dataProduct.length) {
